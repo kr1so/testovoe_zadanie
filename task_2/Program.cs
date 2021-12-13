9143c666a8b360899a5a2a2f0aa0edd1393f8c15
kris.o <yajkakusta@gmail.com>
2021-12-13 21:19:42 +0500

    формулировка задач 2, описание работы

---
+++ task_2/Program.cs
@@ -0,0 +1,45 @@
+﻿// 2. Задача 
+
+// 1. Алгоритм, заполняющий список произвольными целыми чистыми. 
+// Получить новый список из элементов исходного, которые удовлетворяют условиям
+// Элемент кратен 3
+
+
+int[] arrayA = new int[10];
+int[] FillRandomArray(int[] array1)
+{
+    for (int i = 0; i < array1.Length; i++){
+        array1[i] = new Random().Next(0, 15);
+
+    }
+    return array1;
+}
+void PrintArray(int[] arrayPrint)
+{
+    for (int i = 0; i < arrayPrint.Length; i++){
+        Console.Write($"{arrayPrint[i]} ");
+        
+    }
+}
+FillRandomArray(arrayA);
+PrintArray(arrayA);
+// Новый список из элементов исходного, который удовлетворяют условию
+// Элемент кратен 3
+
+
+int[] array2 = new int [10];
+
+int[] EqualityThree(int[] array)
+{
+    for (int i = 0; i < array.Length; i++){
+
+            if (array[i] % 3 == 0) {
+                return array2;
+            }
+            
+    }
+    return array;
+}
+array2= EqualityThree(arrayA);
+System.Console.WriteLine();
+PrintArray(array2);
---
+++ task_4/Program.cs
@@ -0,0 +1,33 @@
+﻿// 2. Задача 
+// 1. Алгоритм, заполняющий список произвольными целыми чистыми. 
+// Получить новый список из элементов исходного, которые удовлетворяют условиям
+// Элемент не кратен 2
+
+int[] arrayA = new int[10];
+int[] FillRandomArray(int[] array1)
+{
+    for (int i = 0; i < array1.Length; i++){
+        array1[i] = new Random().Next(0, 15);
+    }
+    return array1;
+}
+void PrintArray(int[] arrayPrint)
+{
+    for (int i = 0; i < arrayPrint.Length; i++){
+        Console.Write($"{arrayPrint[i]} ");
+    }
+}
+FillRandomArray(arrayA);
+PrintArray(arrayA);
+// Новый список из элементов исходного, который удовлетворяют условию
+// Элемент не кратен 2
+int EqualityThree(int[] array)
+{
+    for (int i = 0; i < array.Length; i++){
+        if (! array[i] % 2 == 0) return array[i];
+    }
+    return 0;
+}
+System.Console.WriteLine(EqualityThree(arrayA));
+System.Console.WriteLine();
+PrintArray(arrayA);
